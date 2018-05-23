c8e86abc79ea66ec8d928cc4171830d96f6287e0
Ian Colwell <icolwell@gsd.uwaterloo.ca>
2018-05-23 19:33:12 -0400

    Custom configure scripts

--- scripts/configure_machine.bash
+++ scripts/configure_machine.bash
@@ -6,17 +6,11 @@ REPO_DIR="$(dirname "$SCRIPT_DIR")"
 
 source "$SCRIPT_DIR/utils.bash"
 
-# Paths to config locations
-PUBLIC_CONFIGS_DIR="$REPO_DIR/configs"
-PRIVATE_CONFIGS_DIR="$HOME/sync/dotfiles/common_configs"
-SYSTEM_CONFIGS_DIR="$HOME/sync/dotfiles/sys_specific_configs/$HOSTNAME"
-
 # Config locations
 CONFIG_DIRS=(
 	"$REPO_DIR/configs"
 	"$HOME/df_sync/configs"
 )
-
 
 main()
 {
@@ -34,9 +28,11 @@ main()
 	remove_stuff
 	link_stuff
 	setup_vim
-	# configure_thunderbird
-	# configure_gsettings
-	# configure_systemd
+
+	echo "Running custom configuration scripts ..."
+	for CUSTOM_CONFIG in "${CUSTOM_CONFIGS[@]}"; do
+		bash "$CUSTOM_CONFIG"
+	done
 
 	echo ""
 	echo "Configuration complete!"
@@ -56,9 +52,6 @@ link_stuff()
 	for CONFIG_DIR in "${CONFIG_DIRS[@]}"; do
 		process_config_container "$CONFIG_DIR"
 	done
-	# process_config_dir "$PUBLIC_CONFIGS_DIR"
-	# process_config_dir "$PRIVATE_CONFIGS_DIR"
-	# process_config_dir "$SYSTEM_CONFIGS_DIR"
 
 	echo "Symlinks created successfully."
 	echo ""
@@ -81,63 +74,6 @@ setup_vim()
 	echo "Vim setup updated."
 }
 
-configure_thunderbird()
-{
-	# Thunderbird
-	TB_PROFILE='Ian'
-	TB_PROFILE_DIR="$HOME/.thunderbird/$TB_PROFILE"
-	if [ -d $TB_PROFILE_DIR ] && hash thunderbird 2>/dev/null ; then
-		# Remove any existing links or files
-		rm -f ~/.thunderbird/Ian/ImapMail/imap-mail.outlook.com/msgFilterRules.dat
-		rm -f ~/.thunderbird/Ian/ImapMail/imap.gmail.com/msgFilterRules.dat
-		rm -f ~/.thunderbird/Ian/ImapMail/connect.uwaterloo.ca/msgFilterRules.dat
-
-		ln -sf "$HOME/sync/dotfiles/thunderbird/imap-mail.outlook.com/msgFilterRules.dat" ~/.thunderbird/Ian/ImapMail/imap-mail.outlook.com/msgFilterRules.dat
-		ln -sf "$HOME/sync/dotfiles/thunderbird/imap.gmail.com/msgFilterRules.dat" ~/.thunderbird/Ian/ImapMail/imap.gmail.com/msgFilterRules.dat
-		ln -sf "$HOME/sync/dotfiles/thunderbird/connect.uwaterloo.ca/msgFilterRules.dat" ~/.thunderbird/Ian/ImapMail/connect.uwaterloo.ca/msgFilterRules.dat
-	else
-		echo "Warning: no thunderbird profile named $TB_PROFILE exists, skipping thunderbird configuration"
-	fi
-}
-
-configure_gsettings()
-{
-	gsettings set com.canonical.Unity.Launcher favorites \
-		"['application://unity-control-center.desktop',
-		'application://gnome-system-monitor.desktop',
-		'application://org.gnome.Nautilus.desktop',
-		'application://opera.desktop',
-		'application://atom.desktop',
-		'unity://expo-icon',
-		'unity://devices',
-		'unity://running-apps']"
-
-	gsettings set org.gnome.nautilus.preferences default-folder-viewer 'list-view'
-	gsettings set org.compiz.unityshell:/org/compiz/profiles/unity/plugins/unityshell/ icon-size 42
-
-	# Disable sticky edges
-	dconf write /org/compiz/profiles/unity/plugins/unityshell/launcher-capture-mouse false
-
-	# Default apps
-	xdg-settings set default-web-browser opera.desktop
-}
-
-configure_systemd()
-{
-	echo "Enabling systemd services ..."
-	# Systemd does not allow symlinks which is quite frustrating
-	# We must place actual service files, not symlinks
-	mkdir -p ~/.config/systemd/user
-	wget -qP ~/.config/systemd/user/ https://raw.githubusercontent.com/syncthing/syncthing/master/etc/linux-systemd/user/syncthing.service
-	systemctl --user enable syncthing.service
-	systemctl --user start syncthing.service
-
-	echo "systemd services enabled."
-}
-
-#------------------------------------------------------------------------------#
-# Anything below this line should not require editing based on user preference
-
 process_config_container()
 {
 	# This function takes in a config container directory and processes any
@@ -158,11 +94,17 @@ process_config_container()
 
 process_config_category()
 {
+	CUSTOM_CONFIGURE_FILE="$1/dotfile_config/configure.bash"
+
 	if [ -d "$1/home" ]; then
 		setup_configs "$1/home" "$HOME"
 	fi
 	if [ -d "$1/root" ]; then
 		setup_configs "$1/root" ""
+	fi
+
+	if [ -f "$CUSTOM_CONFIGURE_FILE" ]; then
+		CUSTOM_CONFIGS+=("$CUSTOM_CONFIGURE_FILE")
 	fi
 }
 
