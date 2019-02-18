f7d3c0a039bee77dc8337bdec28d4d94ab611088
Ian Colwell <icolwell@gsd.uwaterloo.ca>
2019-02-18 10:38:08 -0500

    Install RPiMonitor for all rpis

---
+++ configs/class_configs/rpi/dotfile_config/install.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+
+# RPI monitor install
+curl -sSL https://raw.githubusercontent.com/icolwell/install_scripts/master/rpimonitor_install.bash | bash
--- configs/sys_specific_configs/raspy/dotfile_config/install.bash
+++ configs/sys_specific_configs/raspy/dotfile_config/install.bash
@@ -3,12 +3,3 @@ set -e
 
 # Create dir so that pihole configs will get linked
 mkdir -p /etc/pihole
-
-#TODO: Move this to install_script repo
-# RPI monitor install
-
-sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 2C0D3C0F
-sudo wget http://goo.gl/vewCLL -O /etc/apt/sources.list.d/rpimonitor.list
-sudo apt-get update
-sudo apt-get -y install rpimonitor
-sudo /etc/init.d/rpimonitor update
