cbae300b4266550b3f5bebcb612dfb4301eae7f5
Ian Colwell <icolwell@gsd.uwaterloo.ca>
2022-11-07 21:36:03 -0500

    Remove atom, slack, and docker from default installs

--- configs/class_configs/desktop/dotfile_config/install.bash
+++ configs/class_configs/desktop/dotfile_config/install.bash
@@ -1,50 +1,13 @@
 #!/bin/bash
 set -e
 
-ATOM_PACKAGES=(
-	atom-beautify
-	autocomplete-clang
-	busy-signal
-	clang-format
-	git-time-machine
-	indent-detective
-	intentions
-	language-cmake
-	language-lua
-	linter
-	linter-clang
-	linter-cpplint
-	linter-shellcheck
-	linter-ui-default
-	markdown-pdf
-	minimap
-	sort-lines
-)
-
 custom_installs() {
 	curl -sSL https://raw.githubusercontent.com/icolwell/install_scripts/master/ros_install.bash | bash
 	curl -sSL https://raw.githubusercontent.com/icolwell/install_scripts/master/opera_install.bash | bash
-	curl -sSL https://raw.githubusercontent.com/icolwell/install_scripts/master/atom_install.bash | bash
 	curl -sSL https://raw.githubusercontent.com/icolwell/install_scripts/master/syncthing_install.bash | bash
-	# curl -sSL https://raw.githubusercontent.com/icolwell/install_scripts/master/slack_install.bash | bash
 	curl -sSL https://raw.githubusercontent.com/icolwell/install_scripts/master/spotify_install.bash | bash
-	curl -sSL https://raw.githubusercontent.com/icolwell/install_scripts/master/docker_install.bash | bash
 	curl -sSL https://raw.githubusercontent.com/icolwell/install_scripts/master/signal_install.bash | bash
 	curl -sSL https://raw.githubusercontent.com/icolwell/install_scripts/master/chrome_install.bash | bash
 }
 
-install_atom_packages()
-{
-	ARRAY=("$@")
-	for atmpkg in "${ARRAY[@]}"; do
-		if [[ ! -d "$HOME/.atom/packages/$atmpkg" ]]; then
-			apm install "$atmpkg"
-		else
-			echo "atom package $atmpkg is already installed"
-		fi
-	done
-}
-
 custom_installs
-
-install_atom_packages "${ATOM_PACKAGES[@]}"
